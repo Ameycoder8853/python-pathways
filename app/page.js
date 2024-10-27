a207d222e2eba2747c7ff5b73f6c0a9c00ff87f5
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-10-27 20:12:57 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const Home = () => {
   };
 
   return (
-    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center px-5">
+    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center p-5">
       <h1 className="text-5xl font-bold mb-4">Python Pathways</h1>
       <p className="text-lg mb-8">The Future of Learning is Almost Here</p>
       <div className="w-96 h-12 rounded-full bg-white relative from-red-400 via-red-500 to-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-900 shadow-2xl shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80">
