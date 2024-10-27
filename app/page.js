b7d2be3b9c98430bc5a80b4ea3742b8f6f5f245b
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-10-27 17:57:34 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,13 +14,13 @@ const Home = () => {
     <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl font-bold mb-4">Python Pathways</h1>
       <p className="text-lg mb-8">The Future of Learning is Almost Here</p>
-<div classname="bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80">
+<div classname="bg-gradient-to-r from-red-400 via-red-500 to-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80">
       <div className="w-96 h-12 rounded-full bg-white relative">
 
         <div className="absolute top-0 left-0 w-64 h-12 rounded-full bg-orange-800"></div>
       </div>
 </div>
-      <p className="text-lg mt-8 justify-center items-center">Are you ready to be among the first to experience the future of learning?</p>
+      <p className="text-xl mt-8 justify-center items-center">Are you ready to be among the first to experience the future of learning?</p>
       <p className="text-lg mt-4 text-orange-800 justify-center items-center">
         Reserve your seat at the Python Pathways launch event and be a part of something revolutionary.
       </p>
