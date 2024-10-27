909a143da339ec32403abe3f15e8a4d15b743105
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-10-27 20:46:44 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const Home = () => {
   };
 
   return (
-<div className="w-[100%] bg-white justify-center items-center text-center">
+<div className="w-[100%] bg-white justify-center items-center text-center p-10">
 
     <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
 
